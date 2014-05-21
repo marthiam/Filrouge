b9dcb64c7708573dba57233d63208feb5e26a31b
marthiam <Mariam@wifi-salsa-119.priv.enst-bretagne.fr>
2014-05-21 14:24:02 +0200

    test

---
+++ initialBettingSW/src/fr/uv1/ValidationTests.java
@@ -0,0 +1,34 @@
+package fr.uv1;
+
+import fr.uv1.bettingServices.*;
+import fr.uv1.bettingServices.exceptions.BadParametersException;
+import fr.uv1.tests.validation.*;
+import fr.uv1.tests.validation.withoutPersistence.SecondIncrementValidationTests;
+
+public class ValidationTests {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		new SecondIncrementValidationTests() {
+
+			@Override
+			public Betting plugToBetting() {
+				try {
+					return new BettingSoft("ilesCaimans");
+				} catch (BadParametersException e) {
+					e.printStackTrace();
+				}
+				return null;
+			}
+
+			@Override
+			public String getManagerPassword() {
+				return "ilesCaimans";
+			}
+		};
+
+	}
+}
